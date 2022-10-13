effb4e23c1271d9aaf5de1fcf8aa36b6cde9c16f
Nothung429 <frank.gatto96.fg@gmail.com>
2022-10-13 16:21:35 +0200

    Aggiungere i file di progetto.

---
+++ Text.cs
@@ -0,0 +1,25 @@
+﻿//PARTE 1
+
+//Completiamo le pagine di gestione delle nostre pizze!
+//Abbiamo la pagina con la lista di tutte le pizze, quella con i dettagli della singola pizza, quella per crearla...cosa manca?
+
+//Dobbiamo realizzare :
+//    pagina di modifica di una pizza
+//    cancellazione di una pizza cliccando un pulsante presente nella grafica di ogni singolo prodotto mostrato nella lista in homepage
+
+
+//PARTE 2
+
+//Oggi sviluppiamo un’importante funzionalità : aggiungiamo una categoria alle nostre pizze (“Pizze classiche”, “Pizze bianche”, “Pizze vegetariane”, “Pizze di mare”, ...).
+
+//Dobbiamo quindi predisporre tutto il codice necessario per poter collegare una categoria a una pizza (in una relazione 1 a molti, cioè una pizza può avere una sola categoria, e una categoria può essere collegata a più pizze). Tramite migration dobbiamo creare la tabella per le categorie. Popoliamola a mano con i valori elencati precedentemente.
+
+//Aggiungiamo poi l’informazione della categoria nelle varie pagine :
+//    nei dettagli di una singola pizza (nell’admin) mostrare la sua categoria
+//    quando si crea/modifica una pizza si deve poter selezionare anche la sua categoria
+
+
+//PARTE 3
+
+//Oggi sviluppiamo un’altra importante funzionalità : aggiungiamo gli ingredienti alle nostre pizze.
+//Una pizza può avere più ingredienti, e un ingrediente può essere presente in più pizze.
